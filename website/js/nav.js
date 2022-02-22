2eb310e765622b1d99b406e43a43c5f225ca30d7
Nick Koch <nick.gaston.koch@protonmail.com>
2022-02-22 23:42:57 +0100

    added nav active highlighting

--- website/js/nav.js
+++ website/js/nav.js
@@ -1,12 +1,25 @@
-fetch('nav.html')
-.then(res => res.text())
-.then(text => {
+fetch("nav.html")
+  .then((res) => res.text())
+  .then((text) => {
     let elem = document.querySelector("div#navbar");
     elem.innerHTML = text;
 
     if (location.href.includes("index.html")) {
-        console.log("index.html");
-        var logo = document.getElementById("logo");
-        logo.style.display = "none";
+      console.log("index.html");
+      var logo = document.getElementById("logo");
+      logo.style.display = "none";
     }
-})
+
+    var pathname = document.location.pathname;
+    console.log("the pathname is: ", pathname);
+
+    var nav_items = document.getElementsByClassName("nav-link");
+    console.log(nav_items);
+
+    for (var i = 0; i < nav_items.length; i++) {
+        console.log(nav_items[i].href);
+        if (nav_items[i].href.includes(pathname)) {
+            nav_items[i].className += " active";
+        }
+    }
+  });
