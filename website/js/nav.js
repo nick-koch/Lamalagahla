a68dc9ee744df89d190fc1283ec5a051e6bacd20
Netzpir4t <lovis.st@outlook.com>
2022-03-07 15:57:25 +0100

    Auto Refresh und About Us Title

--- website/js/nav.js
+++ website/js/nav.js
@@ -27,3 +27,13 @@ fetch("nav.html")
         }
     }
   });
+
+
+$(window).bind('resize', function(e)
+{
+  if (window.RT) clearTimeout(window.RT);
+  window.RT = setTimeout(function()
+  {
+    this.location.reload(false); /* false to get page from cache */
+  }, 100);
+});
